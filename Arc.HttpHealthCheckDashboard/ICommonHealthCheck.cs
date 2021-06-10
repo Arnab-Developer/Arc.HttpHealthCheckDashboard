13ee99216ae0aefea1ee246ff25f1027c67afdd5
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-06-10 20:13:04 +0530

    More changes (#1)

--- Arc.HttpHealthCheckDashboard/ICommonHealthCheck.cs
+++ Arc.HttpHealthCheckDashboard/ICommonHealthCheck.cs
@@ -2,8 +2,16 @@
 
 namespace Arc.HttpHealthCheckDashboard
 {
+    /// <summary>
+    /// Common health check class.
+    /// </summary>
     public interface ICommonHealthCheck
     {
+        /// <summary>
+        /// Check the api is healthy or not.
+        /// </summary>
+        /// <param name="apiDetail">Api detail to be checked</param>
+        /// <returns>Returns true if the api is healthy otherwise false</returns>
         bool IsApiHealthy(ApiDetail? apiDetail);
     }
 }
